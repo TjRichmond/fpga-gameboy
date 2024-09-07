48e59850544883a84d0a57e6b4926ccbfaa77fb4
TjRichmond <TjRichmond99@gmail.com>
2024-09-07 13:37:27 -0700

    Added alu.v and update rebuild script

---
+++ docs/research.md
@@ -0,0 +1,5 @@
+# Research Documents
+- https://gbdev.io/pandocs/
+- http://marc.rawer.de/Gameboy/Docs/GBCPUman.pdf
+- http://www1.cs.columbia.edu/~sedwards/classes/2019/4840-spring/reports/GameBoy-presentation.pdf
+- https://www.zilog.com/docs/z80/um0080.pdf
---
+++ src/cpu/alu.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/07/2024 01:36:18 PM
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu(
+    input [7:0] operand1,
+    input [7:0] operand2,
+    input [3:0] operation,
+    output [7:0] result,
+    output zero_flag,
+    output carry_flag
+    );
+endmodule
